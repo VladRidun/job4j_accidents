6ad0b9f1224ca6a9ac32e348190610f161d3c616
VladRidun <ridunvv@gmail.com>
2024-06-14 11:10:01 +0400

    2. Регистрация пользователя

--- src/main/java/ru/job4j/accidents/service/HibernateUserService.java
+++ src/main/java/ru/job4j/accidents/service/HibernateUserService.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
@@ -12,6 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class HibernateUserService implements UserService {
+    private static final Logger LOG = LoggerFactory.getLogger(HibernateUserService.class.getName());
     private final PasswordEncoder encoder;
     private final UserRepository users;
     private final AuthorityRepository authorities;
@@ -23,9 +26,10 @@ public class HibernateUserService implements UserService {
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
         try {
             user = users.save(user);
-        } catch (Exception e) {
-            user = null;
+            return Optional.of(user);
+        } catch (Exception exception) {
+            LOG.info("Пользователь уже зарегистрирован", exception);
         }
-        return Optional.ofNullable(user);
+        return Optional.empty();
     }
 }
