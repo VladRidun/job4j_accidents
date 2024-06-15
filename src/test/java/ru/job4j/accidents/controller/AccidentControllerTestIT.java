dea2b0945b972ff39905874510a99e8e45dfff42
VladRidun <ridunvv@gmail.com>
2024-06-15 17:15:09 +0400

    0. Spring test

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTestIT.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTestIT.java
@@ -2,8 +2,6 @@ package ru.job4j.accidents.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,9 +9,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
-import ru.job4j.accidents.service.AccidentService;
-import ru.job4j.accidents.service.AccidentTypeService;
-import ru.job4j.accidents.service.RuleService;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- src/test/java/ru/job4j/accidents/controller/IndexControllerTestIT.java
+++ src/test/java/ru/job4j/accidents/controller/IndexControllerTestIT.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
